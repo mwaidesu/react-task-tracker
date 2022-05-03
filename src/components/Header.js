daf7b031583305530044c19c6e91f6cf919ece4b
Mwai <machariamwai@gmail.com>
2022-05-03 20:10:38 +0300

    Add show and. hide when Add button clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,24 @@
-import PropTypes from 'prop-types'
-import Button from "./Button"
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({title}) => {
-
-  const onClick =() =>{
-  alert("click")
-}
+const Header = ({ title, onAdd, showAdd }) => {
+  //   const onAdd =() =>{
+  //   //alert("click")
+  // }
   return (
-    <header className = "header">
+    <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add"  onClick={onClick}/>
-  
-
+      <Button color={showAdd? 'red' : 'green'} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
     </header>
-    
-
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: 'Task Tracker'
-}
+  title: "Task Tracker",
+};
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-}
+};
 
-export default Header
+export default Header;
